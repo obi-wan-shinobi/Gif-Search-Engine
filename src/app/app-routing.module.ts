1ca23c1cf9f83fd8b416121a111e70c637729fbc
Shreyas Kalvankar <shreyaskalvankar@gmail.com>
2020-11-24 14:26:15 +0530

    Added routing of home/about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent } from "./sign-up/sign-up.component";
 import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path:'signup', component: SignUpComponent },
-  { path:'login', component: LoginComponent }
+  { path:'login', component: LoginComponent },
+  { path:'home', component: HomeComponent }
 ];
 
 @NgModule({
@@ -14,4 +16,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [SignUpComponent, LoginComponent];
+export const routingComponents = [SignUpComponent, LoginComponent, HomeComponent];
