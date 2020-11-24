1364256333ba02b3f4bf2b27d4cda70182ec7ff1
Shreyas Kalvankar <shreyaskalvankar@gmail.com>
2020-11-24 14:53:50 +0530

    Fixed search component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent } from "./sign-up/sign-up.component";
 import { LoginComponent } from "./login/login.component";
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path:'signup', component: SignUpComponent },
   { path:'login', component: LoginComponent },
-  { path:'home', component: HomeComponent }
+  { path:'home', component: HomeComponent },
+  { path:'search', component: SearchComponent }
 ];
 
 @NgModule({
@@ -16,4 +18,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [SignUpComponent, LoginComponent, HomeComponent];
+export const routingComponents = [SignUpComponent, LoginComponent, HomeComponent, SearchComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SearchComponent,
     routingComponents
   ],
   imports: [
